cb0d379bbd54a9074c9b43364a2367b041610c0a
deependra88 <deependra.gaurav@gmail.com>
2026-01-21 23:33:01 +0530

    multiple events in react

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,13 +3,22 @@ import React,{useState} from "react";
 function Counter(){
 
 const [count,setCount] = useState(0);
+const [factor,setFactor] = useState(1);
 
 function IncCount(){
-    setCount(count+1);
+    setCount(count+factor);
 }
 
 function DecCount(){
-    setCount(count-1);
+    setCount(count-factor);
+}
+
+function IncFactor(){
+    setFactor(factor+1);
+}
+
+function DecFactor(){
+    setFactor(factor-1);
 }
     return(
         <>
@@ -17,6 +26,11 @@ function DecCount(){
 
         <button onClick={IncCount}>Increment</button>
         <button onClick={DecCount}>Decrement</button>
+
+        <h1>The value of the factor: {factor}</h1>
+
+        <button onClick={IncFactor}>Increment</button>
+        <button onClick={DecFactor}>Decrement</button>
         </>
     )
 }
